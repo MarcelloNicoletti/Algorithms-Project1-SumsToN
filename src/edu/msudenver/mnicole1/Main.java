882f711ad811f0c7128c1fd4a0c3ddae5c33dc3d
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-08-23 20:47:12 -0600

    Added recursive base case

--- src/edu/msudenver/mnicole1/Main.java
+++ src/edu/msudenver/mnicole1/Main.java
@@ -1,8 +1,32 @@
 package edu.msudenver.mnicole1;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        sum(5, new LinkedList<>());
+    }
+
+    private static boolean sum (int goal, List<Integer> current) {
+        int sumStart = 1;
+        int currentSum = 0;
+        if (!current.isEmpty()) {
+            sumStart = current.get(current.size() - 1);
+            for (Integer i : current) {
+                currentSum += i;
+            }
+        }
+        if (currentSum == goal) {
+            StringJoiner sj = new StringJoiner("+");
+            for (Integer i : current) {
+                sj.add(i.toString());
+            }
+            System.out.println(sj.toString());
+            return true;
+        }
+        return false;
     }
 }
